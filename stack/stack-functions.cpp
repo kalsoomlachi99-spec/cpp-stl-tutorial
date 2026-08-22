c954c5260652a8635a11a50f143199274a0d261b
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-22 14:45:46 +0500

    Stack function | swap

--- stack/stack-functions.cpp
+++ stack/stack-functions.cpp
@@ -2,21 +2,43 @@
 #include <stack>
 using namespace std;
 
+void line() {
+    cout << "----------------" << endl;
+}
 int main() {
-    /*Non-Sequential Data Stacture | Stack*/
+    /*Non-Sequential Container | Stack*/
     
-    stack <int> s;
+    stack <int> s1;
+    stack <int> s2;
+    
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
 
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
+    cout << "Before swap: " << endl;
+    cout << "size of s1 = " << s1.size() << endl;
+    cout << "size of s2 = " << s2.size() << endl;
 
-    while (!s.empty()){
-        cout << s.top() << " "; 
-        s.pop();
+    
+    line();
+
+    s2.swap(s1);
+
+    cout << "After swap: " << endl;
+    cout << "size of s1 = " << s1.size() << endl;
+    cout << "size of s2 = " << s2.size() << endl;
+
+    line();
+
+    cout << "Values of s2 is: " << endl;
+     while (!s2.empty()){
+        cout << s2.top() << " "; 
+        s2.pop();
     }
     cout << endl;
+
+    line();
 
     return 0;
 }
