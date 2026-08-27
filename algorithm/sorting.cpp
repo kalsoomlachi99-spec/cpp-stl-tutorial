cfe712a4df50921f42c5ea6cb63996453b15a962
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-27 21:26:33 +0500

    Vector sorting

--- algorithm/sorting.cpp
+++ algorithm/sorting.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main() {
     
     /*C++ STL Algorithm | sorting */
-
+    
+    cout << "Sorting in array:- " << endl;
     int arr[] = {3, 0, 8, 2, 1, 5, 3};
     int n = sizeof(arr)/4 ;
 
-    cout << "sorting in array:- " << endl;
     sort (arr, arr + n);
 
     for (int val : arr){
+        cout << val << " ";
+    }
+    cout << endl;
+
+    cout << "Sorting in vector:- " << endl;
+
+    vector <int> vec = {3, 0, 8, 2, 1, 5, 3};
+
+    sort (vec.begin(), vec.end());
+
+    for (int val : vec){
         cout << val << " ";
     }
     cout << endl;
