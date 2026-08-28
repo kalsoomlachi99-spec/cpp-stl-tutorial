6e592f5a3d6ffae874008fff1292e7b9553638a1
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-28 08:46:05 +0500

    Custom comparators

--- algorithm/sorting.cpp
+++ algorithm/sorting.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
+
+bool comparators(pair<int, int> p1, pair <int, int> p2){ //custom comparator sort the second element of pair in accending order
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+
+    if(p1.first < p2.first) return true; // if second elements are same then sort the first element
+    else return false; //if(p1.first > p2.first)
+}
+
+void line(){
+    cout << "-----------------------" << endl;
+}
 
 int main() {
     
@@ -18,6 +29,7 @@ int main() {
         cout << val << " ";
     }
     cout << endl;
+    line();
 
     cout << "Sorting in vector:- " << endl;
 
@@ -29,8 +41,10 @@ int main() {
         cout << val << " ";
     }
     cout << endl;
+    line();
 
     cout << "Sorting in vector of pair:- " << endl;
+    cout << "Sort the first element." << endl;
 
     vector <pair <int, int>> vecOfPair = {{1,2}, {2,3}, {3,1}, {7,1}};
 
@@ -39,6 +53,20 @@ int main() {
     for (auto p : vecOfPair){
         cout << p.first << " " << p.second << endl; //sort according to the first value of pair
     }
+    line();
+
+    cout << "Sorting in vector of pair:- " << endl;
+    cout << "Sort the second element." << endl;
+
+    vector <pair <int, int>> cusVecOfPair = {{1,2}, {2,3}, {3,1}, {7,1}};
+
+    sort (cusVecOfPair.begin(), cusVecOfPair.end(), comparators); 
+
+    for (auto p : cusVecOfPair){
+        cout << p.first << " " << p.second << endl; //sort according to the first value of pair
+    }
+    line();
+
   
     return 0;
 }
